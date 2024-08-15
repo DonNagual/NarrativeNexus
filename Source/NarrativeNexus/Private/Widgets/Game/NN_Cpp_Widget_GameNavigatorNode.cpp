78d75fc77b5a1e150e4eb01b7a5115e4281cff50
DonNagual <stonedbull@gmail.com>
2024-08-15 18:42:14 +0200

    Added functionality to manage nodes in Game Navigator widget
    - Implemented RemoveAllNodesFromScrollBox() to clear all children from NodeScrollBox in NN_Cpp_Widget_GameNavigator.
    - Updated OnBackButtonClicked() to call RemoveAllNodesFromScrollBox() when navigating back to the Game Menu.
    - Added OnRepeatButtonClicked() functionality to remove and re-add nodes to the NodeScrollBox.
    - Implemented NN_Cpp_Widget_GameNavigatorNode with a basic button that logs when clicked.
    - Ensured that the GameNavigatorNode widgets are dynamically added to the NodeScrollBox when GameNavigator becomes visible.
    
    This commit introduces the basic functionality for managing nodes in the Game Navigator widget. Nodes can be dynamically added and removed, providing a flexible interface for navigating different game scenarios. The changes also include logging and stubs for further enhancements.

--- Source/NarrativeNexus/Private/Widgets/Game/NN_Cpp_Widget_GameNavigatorNode.cpp
+++ Source/NarrativeNexus/Private/Widgets/Game/NN_Cpp_Widget_GameNavigatorNode.cpp
@@ -2,3 +2,18 @@
 
 #include "Widgets/Game/NN_Cpp_Widget_GameNavigatorNode.h"
 
+//
+
+void UNN_Cpp_Widget_GameNavigatorNode::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	DescriptionButton->OnClicked.AddUniqueDynamic(this, &UNN_Cpp_Widget_GameNavigatorNode::OnDescriptionButtonClicked);
+
+	UE_LOG(LogTemp, Warning, TEXT("NN_Cpp_Widget_GameNavigatorNode NativeConstruct - Objekt: %p"), this);
+}
+
+void UNN_Cpp_Widget_GameNavigatorNode::OnDescriptionButtonClicked()
+{
+	
+}
