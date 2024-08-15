088f494f41ca01fb84fa1a107fddf661416f15ff
DonNagual <stonedbull@gmail.com>
2024-08-15 19:44:23 +0200

    Initialize GPT on Button Click and Destroy on Widget Destruction
    - Modified `NN_Cpp_Widget_GameNavigatorNode` to initialize GPT when the `DescriptionButton` is clicked.
    - Implemented `NativeDestruct` in `NN_Cpp_Widget_GameNavigatorNode` to destroy the GPT instance when the widget is destroyed.
    - Refactored `NN_Cpp_IF_GPT` interface to manage GPT initialization and destruction in the `PlayerController`.
    - Removed circular dependencies by using the `NN_Cpp_IF_GPT` interface for GPT management.
    - Updated logging to confirm the creation and destruction of the GPT instance.
    
    This commit refines the GPT lifecycle management, ensuring it is initialized on-demand when interacting with the UI and properly cleaned up afterward.

--- Source/NarrativeNexus/Private/Widgets/Game/NN_Cpp_Widget_GameNavigatorNode.cpp
+++ Source/NarrativeNexus/Private/Widgets/Game/NN_Cpp_Widget_GameNavigatorNode.cpp
@@ -2,18 +2,32 @@
 
 #include "Widgets/Game/NN_Cpp_Widget_GameNavigatorNode.h"
 
-//
-
 void UNN_Cpp_Widget_GameNavigatorNode::NativeConstruct()
 {
 	Super::NativeConstruct();
 
 	DescriptionButton->OnClicked.AddUniqueDynamic(this, &UNN_Cpp_Widget_GameNavigatorNode::OnDescriptionButtonClicked);
 
-	UE_LOG(LogTemp, Warning, TEXT("NN_Cpp_Widget_GameNavigatorNode NativeConstruct - Objekt: %p"), this);
+	//UE_LOG(LogTemp, Warning, TEXT("NN_Cpp_Widget_GameNavigatorNode Construct - Objekt: %p"), this);
+}
+
+void UNN_Cpp_Widget_GameNavigatorNode::NativeDestruct()
+{
+	if (auto* Interface = Cast<INN_Cpp_IF_GPT>(GetWorld()->GetFirstPlayerController()))
+	{
+		Interface->DestroyGPT();
+		UE_LOG(LogTemp, Warning, TEXT("GPTInstance destroyed via interface"));
+	}
+
+	Super::NativeDestruct();
 }
 
 void UNN_Cpp_Widget_GameNavigatorNode::OnDescriptionButtonClicked()
 {
-	
+	if (auto* Interface = Cast<INN_Cpp_IF_GPT>(GetWorld()->GetFirstPlayerController()))
+	{
+		Interface->InitializeGPT();
+		UE_LOG(LogTemp, Warning, TEXT("GPTInstance initialized via interface"));
+		UE_LOG(LogTemp, Warning, TEXT("NN_Cpp_Widget_GameNavigatorNode GPTInstance - Objekt: %p"), this);
+	}
 }
